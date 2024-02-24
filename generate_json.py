7069edae5dea00858cf4c2b77bc81dc16d774095
jsgaston <69969971+jsgaston@users.noreply.github.com>
2024-02-24 17:42:57 +0100

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -2,7 +2,13 @@ import os
 import json
 
 folder_path = 'Music'  # Ruta de la carpeta
-music_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+try:
+    music_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-with open('music_list.json', 'w') as json_file:
-    json.dump(music_files, json_file)
+    with open('music_list.json', 'w') as json_file:
+        json.dump(music_files, json_file)
+
+    print("JSON generado exitosamente.")
+except Exception as e:
+    print("Error durante la generación del JSON:", str(e))
+
