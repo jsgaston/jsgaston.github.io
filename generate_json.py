ef0ba9d78319af0a3e85e2df395e00edb16f9f6e
jsgaston <69969971+jsgaston@users.noreply.github.com>
2024-03-25 21:13:45 +0100

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+#
 folder_path = 'Musica'  # Ruta de la carpeta ¿funcionas?
 try:
     music_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
